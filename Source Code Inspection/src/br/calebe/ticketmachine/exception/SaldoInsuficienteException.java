735c1974e71b81f546ec9f57f662b5e7c3430bc1
31542182 <31542182@172.16.19.208>
2019-09-04 20:54:08 -0300

    Test

--- Source Code Inspection/src/br/calebe/ticketmachine/exception/SaldoInsuficienteException.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/exception/SaldoInsuficienteException.java
@@ -5,4 +5,6 @@ package br.calebe.ticketmachine.exception;
  * @author Calebe de Paula Bianchini
  */
 public class SaldoInsuficienteException extends Exception {
+//Apenas testtando o commit
+    
 }
