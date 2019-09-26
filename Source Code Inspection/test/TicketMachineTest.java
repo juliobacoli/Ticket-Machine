2cb52af99b74832efbc94bc2af32b59c8b14453d
juliobacoli <31542182@172.16.18.180>
2019-09-25 21:03:50 -0300

    teste

---
+++ Source Code Inspection/test/TicketMachineTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author 31542182
+ */
+public class TicketMachineTest {
+    
+    public TicketMachineTest() {
+    }
+    
+    @Test
+    public static void inserir() {
+        
+    }
+    
+    
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
